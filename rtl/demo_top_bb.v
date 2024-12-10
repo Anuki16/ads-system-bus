7a33bf65aadc7dbc5a9663017673da1a178e4727
Nuthya Prathibhani <nuthyapr@gmail.com>
2024-12-10 18:27:46 +0530

    Added Demo with bus bridge

---
+++ rtl/demo_top_bb.v
@@ -0,0 +1,170 @@
+module demo_top_bb #(
+	parameter ADDR_WIDTH = 16, 
+	parameter DATA_WIDTH = 8,
+	parameter SLAVE_MEM_ADDR_WIDTH = 12,
+    parameter BB_ADDR_WIDTH = 12,
+    parameter UART_CLOCKS_PER_PULSE = 5208
+)(
+	input clk, rstn,
+
+    input start,
+	output d1_ready,
+	input mode,					// 0 - read, 1 - write
+    input d1_en
+);
+
+
+	wire [DATA_WIDTH-1:0] d1_wdata;   // write data
+	wire [DATA_WIDTH-1:0] d1_rdata; 	// read data
+	wire [ADDR_WIDTH-1:0] d1_addr; 
+    reg d1_valid;
+    reg d1_mode;
+    wire s_ready;     // slaves are ready
+    wire m_u_rx;
+    wire s_u_rx;
+    wire m_u_tx; 
+    wire s_u_tx;
+
+    // Signals connecting to master device
+	wire [ADDR_WIDTH-1:0] daddr;
+    reg [4:0] memaddr;
+    reg memwen;
+
+
+    top_with_bb_v1 #(
+        .ADDR_WIDTH(ADDR_WIDTH), 
+        .DATA_WIDTH(DATA_WIDTH),
+        .SLAVE_MEM_ADDR_WIDTH(SLAVE_MEM_ADDR_WIDTH ),
+        .BB_ADDR_WIDTH(BB_ADDR_WIDTH),
+        .UART_CLOCKS_PER_PULSE(UART_CLOCKS_PER_PULSE)
+    ) bus (
+        .clk(clk), 
+        .rstn(rstn),
+        .d1_wdata(d1_wdata),   
+        .d1_rdata(d1_rdata), 
+        .d1_addr(d1_addr), 
+        .d1_valid(d1_valid), 
+        .d1_ready(d1_ready), 
+        .d1_mode(d1_mode), 
+        .s_ready(s_ready), 
+        .m_u_rx(m_u_rx),
+        .s_u_rx(s_u_rx),
+        .m_u_tx(m_u_tx), 
+        .s_u_tx(s_u_rx)
+    );
+
+    master_bram memory (
+        .address(memaddr),
+        .clock(clk),
+        .data(d1_rdata),
+        .wren(memwen),
+        .q(d1_wdata)
+    );
+
+    wire edge_start;
+    reg start_prev;
+
+    assign d1_start = (edge_start) & d1_en;
+    assign edge_start = (start_prev) & (!start);
+
+    // Buffer the start signal
+    always @(posedge clk) begin
+        if (!rstn) start_prev <= 1'b1;
+        else start_prev <= start; 
+    end
+
+    localparam ADDR = 16'h1001;
+    localparam [4:0] WRITE_OFFSET = 16;
+
+    localparam IDLE = 2'b00,
+            READ = 2'b01,
+            SEND = 2'b10,
+            DONE = 2'b11;
+
+    // State variables
+	reg [1:0] state, next_state;
+    reg [1:0] counter;
+    reg [1:0] idx;
+
+    // Next state logic
+	always @(*) begin
+		case (state)
+			IDLE    : next_state = (start) ? ((!mode) ? SEND : READ) : IDLE;
+			READ    : next_state = (counter == 1) ? SEND : READ;
+			SEND    : next_state = (counter == 1) ? DONE : SEND; 
+            DONE    : next_state = (d1_ready) ? IDLE : DONE;
+			default: next_state = IDLE;
+		endcase
+	end
+
+    // State transition logic
+	always @(posedge clk) begin
+		state <= (!rstn) ? IDLE : next_state;
+	end
+
+    assign ready = (state == IDLE);
+    assign daddr = ADDR;
+
+    always @(posedge clk) begin
+        if (!rstn) begin
+            memaddr <= 'b0;
+            memwen <= 0;
+            d1_valid <= 0;
+            d1_mode <= 0;
+        end 
+        else begin
+            case (state)
+                IDLE : begin
+                    d1_valid <= 0;
+                    memwen <= 0;
+                    counter <= 'b0;
+
+                    if (start) begin
+                        d1_mode <= mode;
+
+                        if (mode) begin     // write to new location, otherwise read from same location
+                            memaddr <= daddr[4:0];
+                        end else begin
+                            memaddr <= WRITE_OFFSET + daddr[4:0];
+                        end
+                        
+                    end else begin
+                        d1_mode <= d1_mode;
+                        memaddr <= memaddr;
+                    end
+                end
+
+                READ : begin
+                    d1_valid <= 0;
+                    counter <= counter ^ 1;
+                end
+
+                SEND : begin
+                    d1_valid <= 1;
+                    counter <= counter ^ 1;
+                end
+
+                DONE : begin
+                    d1_valid <= 0;
+                    if (d1_ready) begin
+                        memwen <= (!d1_mode);
+                    end 
+                    else begin
+                        memwen <= 0;
+                    end
+                end
+
+                default: begin
+                    memaddr <= memaddr;
+                    memwen <= memwen;
+                    d1_valid <= d1_valid;
+                    d1_mode <= d1_mode;
+                    counter <= counter;
+                end
+
+            endcase
+        end
+    end
+
+
+endmodule
